57a8cef93e5aec5f5bbc4e8a6f3914f3deed7e77
Apple <ritvikvij06@gmail.com>
2021-09-04 00:11:55 +0530

    Encryption added

--- cipher.py
+++ cipher.py
@@ -47,6 +47,25 @@ def create_input_vec(input_vec, dim_key):
     return input_matrix
 
 
+#This function encrypts the input message
+def encrypt(input_matrix, key_matrix):
+
+    encrypt_matrix = np.zeros((len(input_matrix), len(input_matrix[0])), dtype=np.int32)
+
+    for i in range(len(input_matrix)):
+        encrypt_matrix[i] = np.squeeze(np.matmul(key_matrix, np.expand_dims(input_matrix[i], axis=1)))
+    
+    #Take modulo to create final encryption
+    encrypt_matrix = np.remainder(encrypt_matrix, 26)
+
+    #Convert encryption to string
+    encrypt_string = ''
+
+    for encrypt_sub_matrix in encrypt_matrix:
+        encrypt_string += ''.join([chr(x+65) for x in encrypt_sub_matrix])
+
+    return encrypt_matrix, encrypt_string
+
 
 if __name__  == "__main__":
 
@@ -56,6 +75,8 @@ if __name__  == "__main__":
 
     input_matrix = create_input_vec(input_vec, key_matrix.shape[0])
 
-    print(input_key)
-    print(key_matrix)
-    print(input_matrix)
+    encrypt_matrix, encrypt_string = encrypt(input_matrix, key_matrix)
+
+    print('String matrix after encryption : ', encrypt_matrix)
+
+    print('Encrypted String : ', encrypt_string)
